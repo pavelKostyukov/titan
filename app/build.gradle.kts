62e9d88092d997f16cc32a3477655a3d26cd89ae
pavelkostyukoff <SpaceSofting@gmail.com>
2024-10-01 02:59:55 +0300

    up sdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.titan7"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
