2441e30f4a72940cefc1db33870bdd272d5c4820
pavelkostyukoff <SpaceSofting@gmail.com>
2024-10-02 01:41:08 +0300

    bugfix fix Mapping

--- app/src/main/java/com/example/titan7/data/Mapper.kt
+++ app/src/main/java/com/example/titan7/data/Mapper.kt
@@ -4,9 +4,9 @@ package com.example.titan7.data
 fun WebResponse.mapToListing() =
     Listing(
         name = this.c,              // Название бумаги
-        symbol = this.name?: "UNKNOWN_SYMBOL" ,//Изменение в процентах относительно цены закрытия
-        price = this.chg,           //Биржа последней сделки
-        change = this.ltp,         //Цена последней сделки
-        previousClose = this.p22,  //Цена последней сделки
-        exchange = this.pcp        //(Изменение цены последней сделки в пунктах относи
+        symbol = this.name,         // Убираем "UNKNOWN_SYMBOL", просто оставляем пустую строку
+        price = this.chg,           // Изменение в процентах относительно цены закрытия
+        change = this.ltp,          // Цена последней сделки
+        previousClose = this.p22,   // Цена последней сделки
+        exchange = this.pcp         // Изменение цены последней сделки в пунктах
     )
