7141cea3bc82c21599dc8b61dbaef8b53b03f846
pavelkostyukoff <SpaceSofting@gmail.com>
2024-10-01 20:31:30 +0300

    logo logic download bugfix

--- app/src/main/java/com/example/titan7/data/QuoteRepositoryImpl.kt
+++ app/src/main/java/com/example/titan7/data/QuoteRepositoryImpl.kt
@@ -26,59 +26,52 @@ open class QuoteRepositoryImpl(private val okHttpClient: OkHttpClient) : QuoteRe
     private val gson = Gson()
     private val datList = mutableListOf<Listing>()
     private val webSocketUrl = "wss://wss.tradernet.com"
-    private val tickersToWatchChanges = listOf(
-        "AFLT",
-        "AAPL.US",
-        "SP500.IDX",
-        "AAPL.US",
-        "RSTI",
-        "GAZP",
-        "MRKZ",
-        "RUAL",
-        "HYDR",
-        "MRKS",
-        "SBER",
-        "FEES",
-        "TGKA",
-        "VTBR",
-        "ANH.US",
-        "VICL.US",
-        "RG.US",
-        "NBL.US",
-        "YETI.US",
-        "WSFS.US",
-        "NIO.US",
-        "DXC.US",
-        "MIC.US",
-        "HSBC.US",
-        "EXPN.EU",
-        "GSK.EU",
-        "SHP.EU",
-        "MAN.EU",
-        "DB1.EU",
-        "MUV2.EU",
-        "TATE.EU",
-        "KGF.EU",
-        "MGGT.EU",
-        "SGGD.EU"
-    )
-
-
-    /**
-     * Получаем котировки
-     * Подписываемся на веб-сокет и возвращаем поток котировок.
-     */
-    var dataValue = MutableStateFlow<List<Listing>>(listOf())
+    private val dataValue = MutableStateFlow<List<Listing>>(listOf())
 
     private fun startWebSocket() {
         val request = Request.Builder().url(webSocketUrl).build()
         datList.clear()
+        val tickersToWatchChanges = listOf(
+            "AFLT",
+            "AAPL.US",
+            "SP500.IDX",
+            "AAPL.US",
+            "RSTI",
+            "GAZP",
+            "MRKZ",
+            "RUAL",
+            "HYDR",
+            "MRKS",
+            "SBER",
+            "FEES",
+            "TGKA",
+            "VTBR",
+            "ANH.US",
+            "VICL.US",
+            "RG.US",
+            "NBL.US",
+            "YETI.US",
+            "WSFS.US",
+            "NIO.US",
+            "DXC.US",
+            "MIC.US",
+            "HSBC.US",
+            "EXPN.EU",
+            "GSK.EU",
+            "SHP.EU",
+            "MAN.EU",
+            "DB1.EU",
+            "MUV2.EU",
+            "TATE.EU",
+            "KGF.EU",
+            "MGGT.EU",
+            "SGGD.EU"
+        )
         val webSocketListener = object : WebSocketListener() {
             override fun onOpen(webSocket: WebSocket, response: Response) {
                 val subscribeMessage =
                     """["realtimeQuotes", ["${tickersToWatchChanges.joinToString("\",\"") { it }}"]]"""
                 webSocket.send(subscribeMessage)
-                Log.d("WebSocket", "Subscribed to realtimeQuotes: $tickersToWatchChanges")
             }
 
             override fun onMessage(webSocket: WebSocket, text: String) {
@@ -117,66 +110,47 @@ open class QuoteRepositoryImpl(private val okHttpClient: OkHttpClient) : QuoteRe
         val newQuote = response.mapToListing()
 
         // Применяем логику замены или уникальности
-        val existingQuotes = datList.map { it.name }
-        // Предполагая, что у Quote есть поле "name"
-        if (!existingQuotes.contains(newQuote.name)) {
-            // Получаем логотип для нового тикера
-            val logoBitmap = newQuote.name?.let {
-                getCompanyLogo(it)
-            }
+        newQuote.logo = newQuote.name?.let { getCompanyLogo(it) }
 
-            newQuote.logo = logoBitmap // Присваиваем иконку котировке
-
+        //Обновляем список данных,
+        val existingIndex = datList.indexOfFirst { it.name == newQuote.name }
+        if (existingIndex == -1) {
+            // Добавляем котировку
             datList.add(newQuote)
         } else {
-            // Обновляем существующую котировку
-            val index = existingQuotes.indexOf(newQuote.name)
-            if (index != -1) {
-                datList[index] = newQuote
-                // Обновляем иконку, если необходимо
-                datList[index].logo = newQuote.logo ?: datList[index].logo
-            }
+            // Обновляем иконку, если необходимо
+            datList[existingIndex] = newQuote
         }
 
         // Обновляем состояние
         dataValue.value = datList.toList()
     }
 
-    override suspend fun startSocket() {
-        startWebSocket()
-    }
-
     private suspend fun getCompanyLogo(ticker: String): Bitmap? {
         return withContext(Dispatchers.IO) {
-            val logoUrl =
-                "https://tradernet.com/logos/get-logo-by-ticker?ticker=${ticker.lowercase()}"
+            val logoUrl = "https://tradernet.com/logos/get-logo-by-ticker?ticker=${ticker.lowercase()}"
             val request = Request.Builder().url(logoUrl).build()
-            val response: Response = okHttpClient.newCall(request).execute()
+            val response = okHttpClient.newCall(request).execute()
 
-            // Логируем тип контента для отладки
             val contentType = response.header("Content-Type")
-            Log.d("LogoRequest", "Content-Type for $ticker: $contentType")
+            Log.d("LogoRequest", "Content-Type $ticker: $contentType")
 
-            return@withContext if (response.isSuccessful) {
-                // Проверяем, что это изображение
-                if (contentType != null && contentType.startsWith("image/")) {
-                    // Читаем байты изображения
-                    response.body?.byteStream()?.use { inputStream ->
-                        // Декодируем изображение из потока
-                        BitmapFactory.decodeStream(inputStream)
-                    }
-                } else {
-                    Log.e("LogoRequest", "Unexpected content type: $contentType")
+            if (response.isSuccessful && contentType != null && contentType.startsWith("image/")) {
+                response.body?.byteStream()?.use { inputStream ->
+                    BitmapFactory.decodeStream(inputStream)
+                } ?: run {
+                    Log.e("LogoRequest", "Error $ticker")
                     null
                 }
             } else {
-                Log.e(
-                    "LogoRequest",
-                    "Failed to load logo for $ticker with status: ${response.code}"
-                )
+                Log.e("LogoRequest", "$ticker status: ${response.code}")
                 null
             }
         }
+    }
+
+    override suspend fun startSocket() {
+        startWebSocket()
     }
 
     override val updateDate: StateFlow<List<Listing>>
